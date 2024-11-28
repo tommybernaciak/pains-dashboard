3f016a35ae2db2c90e8640b2a48c3d699be0d447
Tommy Bernaciak <contact@tommybernaciak.com>
2024-11-28 13:21:52 +0100

    init

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+      <p>{count}</p>
+    </div>
+  );
+}
+
+export default App;
